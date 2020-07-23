7a85ddba8377f862ba3140cdeeb0a635a8dabb9b
Brad Anderson <btanders@pm.me>
2020-07-23 12:11:34 -0700

    App tracks authentication in state and prevents memory leak

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
@@ -7,19 +7,40 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={ HomePage } />
-        <Route path='/shop' component={ ShopPage } />
-        <Route path='/signin' component={ SignInAndSignUpPage } />
-      </Switch>
-    </div>
-  );
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  // Memory leaks
+  unsubscribeFromAuth = null
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    })
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
+  render () {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/' component={ HomePage } />
+          <Route path='/shop' component={ ShopPage } />
+          <Route path='/signin' component={ SignInAndSignUpPage } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
