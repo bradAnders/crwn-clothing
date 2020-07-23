e2a2b09a649aa1e70889da7d0d82d5460edaaf18
Brad Anderson <btanders@pm.me>
2020-07-23 16:28:43 -0700

    cart icon with number inside

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import './header.styles.scss';
 
@@ -32,6 +33,7 @@ const Header = ({ currentUser }) => (
           </Link>
         )
       }
+      <CartIcon />
     </div>
   </div>
 );
