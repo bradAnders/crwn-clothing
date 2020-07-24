4288073b7dd6e061527143c6acdfc8b6fb9ccb97
Brad Anderson <btanders@pm.me>
2020-07-24 10:20:00 -0700

    shop subpages populate with dynamic selector (memoized with lodash)

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import './collection.styles.scss';
 
 // CollectionPage is a Route in ShopPage.js, so we get match automatically
-const CollectionPage = ({ match: { params: { collectionId }} }) => (
+const CollectionPage = ({ match: { params: { collectionId }}, collection: { items } }) => (
   <div className="category">
     <h2>Collection Page { collectionId }</h2>
-    {/* <CollectionItem */}
+    {
+      items.map(
+        item => <CollectionItem  key={ item.id } item={ item } />
+      )
+    }
   </div>
 );
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
  
-export default CollectionPage;
+export default connect(mapStateToProps)(CollectionPage);
