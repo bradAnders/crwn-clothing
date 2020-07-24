80bb2da0a0f02ebf601d2ad70ead90548d763235
Brad Anderson <btanders@pm.me>
2020-07-24 09:51:18 -0700

    passing route parameter to nested routes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { Route } from 'react-router-dom';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CategoryPage from '../category/category.component'
 
 import './shop.styles.scss'
 
-const ShopPage = () => (
+
+// ShopPage is a Route in App.js, so we get match automatically
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <Route exact path={ `${ match.path }` } component={ CollectionsOverview } />
+    {/* categoryId is passed as a parameter to CategoryPage ({ match.params.categoryId })*/}
+    <Route path={ `${ match.path }/:categoryId` } component={ CategoryPage } />
   </div>
 );
 
