b240aa8fa119928e004a9ba6f1fce94bceb33355
Brad Anderson <btanders@pm.me>
2020-07-24 09:34:50 -0700

    moved components around to prepare for subpage routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 import './shop.styles.scss'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
   <div className='shop-page'>
-  {
-    collections.map(({ id, ...otherProperties }) => (
-      <CollectionPreview key={ id } { ...otherProperties } />
-    ))
-  }
+    <CollectionsOverview />
   </div>
 );
 
-
-
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
- 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
