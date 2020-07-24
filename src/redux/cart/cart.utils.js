edb9206d01b35a09c3a67843baea307a260c06fa
Brad Anderson <btanders@pm.me>
2020-07-23 20:28:48 -0700

    arrows in checkout change cart quantities

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,6 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const existingCartItem = cartItems.find(cartItem => 
-    cartItem.id === cartItemToAdd.id
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
   );
 
   if (existingCartItem) {
@@ -11,5 +11,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     )
   } else {
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+  }
+}
+
+
+export const removeItemFromCart =  (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity > 1) {
+    return cartItems.map(cartItem => 
+      cartItem.id === cartItemToRemove.id ?
+      { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+    )
+  } else {
+    return cartItems.filter(
+      cartItem => cartItem.id !== cartItemToRemove.id
+    )
   }
 }
