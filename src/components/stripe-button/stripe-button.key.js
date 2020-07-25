b114524800433ed582d69268b11bdc08176400fc
Brad Anderson <btanders@pm.me>
2020-07-24 17:04:57 -0700

    adding public keys to repo

---
+++ src/components/stripe-button/stripe-button.key.js
@@ -0,0 +1,3 @@
+const publicKey = 'pk_test_51H8WczI9dvQeaxRzuHwO91C7LX33f0iyY5DldbFUfNEfhC3sBc1lVQIqHyz4HHSF7EaJLVjYYCvcJtmSXyTmWhYT00LsrQjdUq';
+
+export default publicKey
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig  = {
+  apiKey: "AIzaSyDz_wDTsMWqT3VDGMs0HbkBsBNVC2qwgWo",
+  authDomain: "crwn-clothing-db-4dd27.firebaseapp.com",
+  databaseURL: "https://crwn-clothing-db-4dd27.firebaseio.com",
+  projectId: "crwn-clothing-db-4dd27",
+  storageBucket: "crwn-clothing-db-4dd27.appspot.com",
+  messagingSenderId: "722597525463",
+  appId: "1:722597525463:web:9930117a549100d0e37d92",
+  measurementId: "G-CEYT2WZ8JN"
+};
+
+export default firebaseConfig;
