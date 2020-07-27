1cabcb98198288056a96c6f265ccc8de3787d598
Brad Anderson <btanders@pm.me>
2020-07-26 23:17:56 -0700

    react hook: componentDidMount and componentWillUnmount example

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
@@ -13,21 +13,31 @@ import {
 
 // CollectionPage is a Route in ShopPage.js, so we get match automatically
 const CollectionPage = ({ collection }) => {
+  useEffect(() => {
+    // Setup code
+    console.log('CollectionPage did mount')
+
+    return () => {
+      // Cleanup code
+      console.log('CollectionPage will unmount')
+    }
+  }, []);
   const { title, items } = collection
   return (
     <CollectionPageContainer>
-      <CollectionTitle>{ title }</CollectionTitle>
+      <CollectionTitle>{title}</CollectionTitle>
       <CollectionItemsContainer>
         {items.map(
-          item => <CollectionItem  key={ item.id } item={ item } />
+          item => <CollectionItem key={item.id} item={item} />
         )}
       </CollectionItemsContainer>
     </CollectionPageContainer>
   )
-;}
+    ;
+}
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
- 
+
 export default connect(mapStateToProps)(CollectionPage);
