366805ede51e7fa87e44b34e21df73df5ec45668
Brad Anderson <btanders@pm.me>
2020-07-27 21:23:14 -0700

    React Contexts: Context.Consumer

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionsContext from "../../contexts/collections/collections.contexts";
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import {
   CollectionPageContainer,
   CollectionTitle,
-  CollectionItemsContainer
-} from './collection.styles';
+  CollectionItemsContainer,
+} from "./collection.styles";
 
 // CollectionPage is a Route in ShopPage.js, so we get match automatically
-const CollectionPage = ({ match, collection }) => {
-  const { title, items } = collection
+const CollectionPage = ({ match }) => {
   return (
-    <CollectionPageContainer>
-      <CollectionTitle>{ title }</CollectionTitle>
-      <CollectionItemsContainer>
-        {items.map(
-          item => <CollectionItem  key={ item.id } item={ item } />
-        )}
-      </CollectionItemsContainer>
-    </CollectionPageContainer>
-  )
-;}
+    <CollectionsContext.Consumer>
+      {(collections) => {
+        const collection = collections[match.params.collectionId];
+        const { title, items } = collection;
+        return (
+          <CollectionPageContainer>
+            <CollectionTitle>{title}</CollectionTitle>
+            <CollectionItemsContainer>
+              {items.map((item) => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </CollectionItemsContainer>
+          </CollectionPageContainer>
+        );
+      }}
+    </CollectionsContext.Consumer>
+  );
+};
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
- 
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
