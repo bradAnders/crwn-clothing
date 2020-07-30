9b5d3911bf1cff295d01fc499a5893d5a8ad26bc
Brad Anderson <btanders@pm.me>
2020-07-29 20:34:39 -0700

    Performance: Added lazy loading

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,36 +1,44 @@
-import React, { useEffect } from 'react';
-import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useEffect, lazy, Suspense } from "react";
+import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+import Spinner from "../../components/spinner/spinner.component";
+
+const CollectionsOverviewContainer = lazy(() =>
+  import("../../components/collections-overview/collections-overview.container")
+);
+const CollectionPageContainer = lazy(() =>
+  import("../collection/collection.container")
+);
 
 // ShopPage is a Route in App.js, so we get match automatically
 const ShopPage = ({ fetchCollectionsStart, match }) => {
-
   useEffect(() => {
-    fetchCollectionsStart()
+    fetchCollectionsStart();
   }, [fetchCollectionsStart]);
 
   return (
-    <div className='shop-page'>
-      <Route
-        exact path={`${match.path}`}
-        component={CollectionsOverviewContainer}
-      />
-      {/* collectionId is passed as a parameter to CollectionPage ({ match.params.collectionId })*/}
-      <Route
-        path={`${match.path}/:collectionId`}
-        component={CollectionPageContainer}
-      />
+    <div className="shop-page">
+      <Suspense fallback={<Spinner />}>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        {/* collectionId is passed as a parameter to CollectionPage ({ match.params.collectionId })*/}
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </Suspense>
     </div>
   );
-}
+};
 
-const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
-})
+const mapDispatchToProps = (dispatch) => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
+});
 
 export default connect(null, mapDispatchToProps)(ShopPage);
