afc0d5676a0112f1aea2c879eaf0153135b0ba8c
Brad Anderson <btanders@pm.me>
2020-07-30 11:59:02 -0700

    PWA: Added PWA files, (bug: serviceWorker is not registering)

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store, persistor } from './redux/store';
+import { store, persistor } from "./redux/store";
+import * as serviceWorker from "./service-worker";
 
-import App from './App';
-import './index.css';
-
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
-  <Provider store={ store } >
+  <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <PersistGate persistor={ persistor } >
+        <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+serviceWorker.register();
